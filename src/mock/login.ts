4ee38f8c740948052570ebe3af04110fbf1d6c5b
shkmzzh <3081764857@qq.com>
2023-05-08 00:07:15 +0800

    修改login

--- src/mock/login.ts
+++ src/mock/login.ts
@@ -1,6 +1,29 @@
 // 根据角色动态生成路由
 import { MockMethod } from "vite-plugin-mock";
 
+const permissionRouter ={
+    path: "/permission",
+    component:'Layout',
+    meta:{
+    title: "权限路由",
+    icon: "setting",
+    hidden:true,
+    roles:"[]",
+    keepAlive:true
+    },
+    children:[
+        {}
+    ]
+}
 export default [
-  
+    {
+        url: "/getAsyncRoutes",
+        method: "get",
+        response: () => {
+          return {
+            success: true,
+            data: [permissionRouter]
+          };
+        }
+      }
 ] as MockMethod[];

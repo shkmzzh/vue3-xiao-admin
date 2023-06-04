1055c22d793962b8d070c793508a756f542f5a5a
shkmzzh <3081764857@qq.com>
2023-06-04 20:40:34 +0800

    poxy

--- dist/assets/index-47be1053.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{e as c,f as o}from"./index-0169ebcb.js";const r={};function n(t,s){return c(),o("h1",null,"welcome")}const f=e(r,[["render",n]]);export{f as default};
--- dist/assets/kun-02eb9b9d.js
+++
@@ -1,1 +0,0 @@
-import{d as e,e as n,f as o}from"./index-0169ebcb.js";const s=e({__name:"kun",setup(t){return(a,r)=>(n(),o("div",null,"测试demo"))}});export{s as default};

c0c82032018e1428d954d67f4f123941cd360fa7
xiao <87223862+shkmzzh@users.noreply.github.com>
2023-12-16 10:20:31 +0800

    优化 request 网络请求

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,9 +1,8 @@
-import axios, { InternalAxiosRequestConfig, AxiosResponse,AxiosRequestConfig } from 'axios'
+import axios, { InternalAxiosRequestConfig, AxiosResponse, AxiosRequestConfig } from 'axios'
 import { useUserStore } from '@/store'
 import { uploadToken } from '@/api/auth/index'
 
 let isRefreshing = false // 控制是否恢复 token
-let retryReqs: any[] = []
 // 创建 axios 实例
 const service = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_API,
@@ -32,8 +31,6 @@ service.interceptors.request.use(
 // 响应拦截器
 service.interceptors.response.use(
   (response: AxiosResponse) => {
-    console.log(response)
-
     if (response.data.code === 200) {
       return response.data
     }
@@ -45,85 +42,23 @@ service.interceptors.response.use(
     if (responseError.status === 401) {
       // 判断 refreshToken token 的过期时间是否到了,过期了需重新登录
       const userStore = useUserStore()
-      console.log(userStore.userInfo.refExpTime, Date.now())
-      console.log(userStore.userInfo.refExpTime <= Date.now())
       if (userStore.userInfo.refExpTime <= Date.now()) {
-        debugger
         ElMessage.error('登录过期需重新登录!')
       } else if (!isRefreshing) {
-        // debugger
-        // // 否则调用 token更新 接口 恢复token
-
-        // console.log('666666')
-        // console.log(userStore.userInfo.refreshToken)
-        // uploadToken().then((res)=>{
-        //   if (res.code === 200) {
-        //     // 更新 token
-        //     userStore.changeUpdateToken(res.data.accessToken, res.data.refreshToken)
-        //     //需求 让 accessToken 过期期间请求失败的接口 重新发起请求
-        //     retryRequest.trigger(res.data.accessToken) // 发布
-        //   }
-
-        //   console.log(res)
-        //   debugger
-        // }).catch((err)=>{}).finally(()=>{
-        //   debugger
-        //   isRefreshing = false
-        // })
-        //   try {
-        //     debugger
-        //     isRefreshing = true
-        //     const res = await uploadToken()
-        //     if (res.code === 200) {
-        //       // 更新 token
-        //       userStore.changeUpdateToken(res.data.accessToken, res.data.refreshToken)
-        //       //需求 让 accessToken 过期期间请求失败的接口 重新发起请求
-        //       retryRequest.trigger(res.data.accessToken) // 发布
-        //     }
-        //   } catch (error) {
-        //   } finally {
-        //     isRefreshing = false
-        //   }
-        // } else {
-        //   debugger
-        // retryRequest.listen(responseError)
-
         try {
-          debugger
-          isRefreshing =true
+          isRefreshing = true
           const res = await uploadToken()
-          if(res?.code===200){
+          if (res?.code === 200) {
             userStore.changeUpdateToken(res.data.accessToken, res.data.refreshToken)
-            debugger
-            // retryRequest.trigger(res.data.accessToken)
-            for (let i = 0, len = retryReqs.length; i < len; i++) {
-              debugger
-              retryReqs[i](res.data.accessToken)
-            }
-            // 队列请求完成，清空
-            retryReqs = []
-            // 返回触发 401 接口正常结果
-            responseError.config.headers = { ...responseError.config.headers, Authorization: res.data.accessToken }
-            return await service(responseError.config)
+            retryRequest.trigger(res.data.accessToken, responseError.config)
           }
         } catch (error) {
-          console.log(error);
-          
+          console.log(error)
         } finally {
           isRefreshing = false
         }
       } else {
-        console.log('1111');
-        debugger
-        console.log('1111');
-        return new Promise((resolve) => {
-          retryReqs.push((token: string) => {
-            debugger
-            responseError.config.headers = { ...responseError.config.headers, Authorization: token }
-            resolve(service(responseError.config))
-          })
-        })
-       return retryRequest.listen(responseError)
+        return retryRequest.listen(responseError)
       }
     } else {
       ElMessage.error(error.response.data.error || '系统出错')
@@ -140,18 +75,20 @@ class RetryOldRequest {
     debugger
     return new Promise((resolve: (value: any) => void) => {
       this.requestQuery.push((newToken: string) => {
-        config.headers={...config.headers,Authorization:newToken}
-        resolve(service( config))
+        config.headers = { ...config.headers, Authorization: newToken }
+        resolve(service(config))
       })
     })
   }
 
-  public trigger(newToken: string): void {
-   
-    // this.requestQuery.forEach((fn) => {
-    //   fn(newToken)
-    // })
+  public async trigger(newToken: string, config: AxiosResponse<any>): Promise<void> {
+    for (let i = 0, fn; (fn = this.requestQuery[i++]); ) {
+      fn(newToken)
+    }
+
     this.requestQuery = []
+    config.headers = { ...config.headers, Authorization: newToken }
+    return await service(config)
   }
 }
 const retryRequest = new RetryOldRequest()
